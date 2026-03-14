4176e5b2eaca4ef098c23aa46f49199c4020f831
Dzvelocity <dzikrimurtadlo@gmail.com>
2026-03-14 12:09:06 +0700

    feat: add database session management and dockerfile for AWS deployment

--- app/db/session.py
+++ app/db/session.py
@@ -1,8 +1,13 @@
 from sqlmodel import create_engine, Session
 from app.core.config import settings
 
-engine = create_engine(settings.DATABASE_URL, pool_pre_ping=True)
+engine = create_engine(
+    settings.DATABASE_URL, 
+    echo=False,           
+    pool_pre_ping=True    
+)
 
+# dipakai di setiap endpoint API
 def get_session():
     with Session(engine) as session:
         yield session
